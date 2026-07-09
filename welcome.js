d5358cc2d06ab919124079c25229332697086b3b
shaiksaniyamehazabeen-a11y <shaiksaniyamehazabeen@gmail.com>
2026-07-09 13:31:23 +0530

    Completed Module 5 Assignment 1

---
+++ welcome.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+const today = new Date();
+console.log(chalk.green("QuickBite Backend is Ready!"));
+console.log(chalk.green("Current Date and Time:"));
+console.log(chalk.green(today.toString()));
